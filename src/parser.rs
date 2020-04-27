77e28ff0b06ec62fc8458dd3ce70152d2fc6d87e
Max Orok <maxwellorok@gmail.com>
2020-04-27 18:45:16 -0400

    parse header with tag nom parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,9 @@
 use crate::*;
 use std::path::PathBuf;
-use nom::*;
 use thiserror::Error;
+
+use nom::*;
+use nom::bytes::complete::tag;
 
 #[derive(Error, Debug)]
 pub enum MshError {
@@ -16,8 +18,16 @@ pub type MshResult<T> = std::result::Result<T, MshError>;
 
 impl Msh2 {
     fn from_file(file: &PathBuf) -> MshResult<Msh2> {
+        let filestr = std::fs::read_to_string(&file).expect("IO error");
+        let input = parse_header(&filestr).unwrap();
+        std::dbg!(input);
         todo!();
     }
+}
+
+fn parse_header(input: &str) -> IResult<&str, ()> {
+    let (input, _) = tag("$MeshFormat\r\n2.2 0 8\r\n$EndMeshFormat")(input)?;
+    Ok((input, ()))
 }
 
 #[cfg(test)]
