406f6e24eec094e27b3a08c843256006ddb8e143
Max Orok <maxwellorok@gmail.com>
2020-04-30 10:50:38 -0400

    Added v4.1 header test

--- src/parser.rs
+++ src/parser.rs
@@ -174,6 +174,14 @@ mod tests {
         assert_debug_snapshot!(parse_header(&std::fs::read_to_string(&path).unwrap()).unwrap().1);
     }
 
+    #[test]
+    fn msh4_ascii_header() {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("props");
+        path.push("v4");
+        path.push("empty.msh");
+        assert_debug_snapshot!(parse_header(&std::fs::read_to_string(&path).unwrap()).unwrap().1);
+    }
     //#[test]
     //fn empty_mesh() {
     //    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
