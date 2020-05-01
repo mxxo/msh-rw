16862dbaf9370e984c28955c7d43a4fd2005e1d1
Max Orok <maxwellorok@gmail.com>
2020-05-01 12:45:01 -0400

    add serde test

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
+## ⓜⓢⓗ🅟🅐🅡🅢🅔🅡 
+A Gmsh mesh file parser. 
 
-
-## `serde` integration 
+### `serde` integration 
 Enable `serde` support with
 ```shell 
 cargo build --features serde 
+```
+Run the `serde` tests with 
+```shell 
+cargo test --features serde 
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -75,5 +75,23 @@ pub enum MeshShape {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use insta::{assert_debug_snapshot, assert_display_snapshot};
 
+    #[test]
+    fn msh_json() {
+        let mut msh = Msh::new();
+        msh.nodes = vec![
+            Point { tag: 1, x: 0.0, y: 0.0, z: 0.0, },
+            Point { tag: 2, x: 1.0, y: 0.0, z: 0.0, },
+        ];
+        msh.elts = vec![MeshElt {
+                tag: 1,
+                ty: MeshShape::Line,
+                nodes: vec![1, 2],
+                physical_groups: None,
+            }
+        ];
+        assert_display_snapshot!(serde_json::to_string(&msh).unwrap());
+    }
 }
